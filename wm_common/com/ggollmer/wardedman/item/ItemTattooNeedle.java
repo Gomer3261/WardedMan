77b6fd5bb0d31d3718afbe48c845b31febe67b2e
Geoff Gollmer <Geoff.Gollmer@Gmail.com>
2013-12-13 00:52:50 -0800

    New gui for tattoo needle

--- wm_common/com/ggollmer/wardedman/item/ItemTattooNeedle.java
+++ wm_common/com/ggollmer/wardedman/item/ItemTattooNeedle.java
@@ -1,12 +1,7 @@
 package com.ggollmer.wardedman.item;
 
-import net.minecraft.item.ItemStack;
-
 import com.ggollmer.wardedman.WardedMan;
 import com.ggollmer.wardedman.lib.ItemNames;
-
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemTattooNeedle extends ItemWardedMan
 {
@@ -16,10 +11,4 @@ public class ItemTattooNeedle extends ItemWardedMan
 		setUnlocalizedName(ItemNames.TATTOO_NEEDLE_NAME);
 		setCreativeTab(WardedMan.tabsWardedMan);
 	}
-	
-	@SideOnly(Side.CLIENT)
-    public boolean hasEffect(ItemStack par1ItemStack)
-    {
-        return true;
-    }
 }
--- wm_common/com/ggollmer/wardedman/item/WardedManItems.java
+++ wm_common/com/ggollmer/wardedman/item/WardedManItems.java
@@ -6,6 +6,8 @@ import com.ggollmer.wardedman.lib.ItemNames;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 import net.minecraft.item.Item;
+import net.minecraft.util.WeightedRandomChestContent;
+import net.minecraftforge.common.ChestGenHooks;
 
 public class WardedManItems
 {
@@ -16,5 +18,6 @@ public class WardedManItems
 		tattooNeedle = new ItemTattooNeedle(ItemIds.TATTOO_NEEDLE);
 		
 		GameRegistry.registerItem(tattooNeedle, ItemNames.TATTOO_NEEDLE_NAME);
-	}	
+		ChestGenHooks.addItem(ChestGenHooks.DUNGEON_CHEST, new WeightedRandomChestContent(tattooNeedle.itemID, 1, 1, 3, 2));
+	}
 }
